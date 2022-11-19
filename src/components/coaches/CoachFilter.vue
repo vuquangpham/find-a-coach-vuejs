7ee6092c185db3166a620c40550587de8fbca2e4
vupham <vuquangpham2909@gmail.com>
2022-11-19 23:41:52 +0700

    :construction: CoachFilter > add filtering function

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,71 @@
+<template>
+  <BaseCard>
+    <h2>Find Your Coach</h2>
+    <form @submit.prevent>
+      <span class="filter-option">
+        <input type="checkbox" id="frontend" checked @change="setFilter">
+        <label for="frontend">Frontend</label>
+      </span>
+      <span class="filter-option">
+        <input type="checkbox" id="backend" checked @change="setFilter">
+        <label for="backend">Backend</label>
+      </span>
+      <span class="filter-option">
+        <input type="checkbox" id="career" checked @change=setFilter>
+        <label for="career">Career</label>
+      </span>
+    </form>
+  </BaseCard>
+</template>
+<script>
+import BaseCard from "@/components/ui/BaseCard";
+
+export default {
+  components: {BaseCard},
+  emits: ['change-filter'],
+  data(){
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        career: true
+      }
+    };
+  },
+  methods: {
+    setFilter(e){
+      const inputId = e.target.id;
+      const isActive = e.target.checked;
+      const updateFilters = {
+        ...this.filters,
+        [inputId]: isActive
+      };
+      this.filters = updateFilters;
+      this.$emit('change-filter', updateFilters);
+    }
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  margin:0.5rem 0;
+}
+
+.filter-option {
+  margin-right:1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align:middle;
+}
+
+.filter-option label {
+  margin-left:0.25rem;
+}
+
+.filter-option.active label {
+  font-weight:bold;
+}
+</style>
--- src/views/coaches/CoachesList.vue
+++ src/views/coaches/CoachesList.vue
@@ -1,5 +1,7 @@
 <template>
-  <section>Filter</section>
+  <section>
+    <CoachFilter @change-filter="setFilters"/>
+  </section>
   <section>
     <BaseCard>
       <div class="controls">
@@ -8,13 +10,13 @@
       </div>
       <ul v-if="hasCoaches">
         <CoachItem
-          v-for="coach in filteredCoaches"
-          :key="coach.id"
-          :id="coach.id"
-          :firstName="coach.firstName"
-          :lastName="coach.lastName"
-          :areas="coach.areas"
-          :rate="coach.hourlyRate"
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :firstName="coach.firstName"
+            :lastName="coach.lastName"
+            :areas="coach.areas"
+            :rate="coach.hourlyRate"
         />
       </ul>
       <h3 v-else>No coaches found.</h3>
@@ -26,33 +28,56 @@
 import CoachItem from "@/components/coaches/CoachItem.vue";
 import BaseCard from "@/components/ui/BaseCard.vue";
 import BaseButton from "@/components/ui/BaseButton.vue";
+import CoachFilter from "@/components/coaches/CoachFilter";
 
 export default {
   components: {
+    CoachFilter,
     CoachItem,
     BaseCard,
     BaseButton,
   },
+  data(){
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      }
+    };
+  },
   computed: {
-    filteredCoaches() {
-      return this.$store.getters["coaches/coaches"];
+    filteredCoaches(){
+      const coaches = this.$store.getters["coaches/coaches"];
+      console.log(coaches, this.activeFilters);
+      return coaches.filter(coach => {
+        if(this.activeFilters.frontend && coach.areas.includes('frontend')) return true;
+        if(this.activeFilters.backend && coach.areas.includes('backend')) return true;
+        if(this.activeFilters.career && coach.areas.includes('career')) return true;
+        return false;
+      });
     },
-    hasCoaches() {
+    hasCoaches(){
       return this.$store.getters["coaches/hasCoaches"];
     },
   },
+  methods: {
+    setFilters(updatedFilters){
+      this.activeFilters = updatedFilters;
+    }
+  }
 };
 </script>
 
 <style scoped>
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+  list-style:none;
+  margin:0;
+  padding:0;
 }
 
 .controls {
-  display: flex;
-  justify-content: space-between;
+  display:flex;
+  justify-content:space-between;
 }
 </style>
